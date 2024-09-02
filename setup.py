f1f4ff679abeda00921ebce95404fbb08c87d81c
joshirishi <joshi.rishikesh@gmail.com>
2024-09-02 18:12:18 +0530

    update
    setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="llmx",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "llmx": ["configs/*.yml"],
+    },
+    install_requires=[
+        # List your dependencies here
+        "requests",
+        # Add any other required packages
+    ],
+)
